6a00cebde81629e88399d7943c5bec1c7ba7bf1b
Raymond Lo <raymond.lo@samsungnext.com>
2020-03-23 14:06:26 -0700

    init

--- docker/default_sdk/build_docker.sh
+++ docker/default_sdk/build_docker.sh
@@ -2,16 +2,15 @@
 #You can register at https://hub.docker.com/
 
 #build the docker image, will take a while and this requires internet connection.
-docker build -t disco_default_sdk -f default.dockerfile .
+docker build -t default_sdk -f default.dockerfile .
 
 #run "docker images" command
 #And you will see this as expected output. 
 #REPOSITORY                     TAG                 IMAGE ID            CREATED             SIZE
-#raymondlo84/disco_python_sdk   latest              d9bf99007535        43 seconds ago      361MB
-#python                         3.7-slim-buster     69afd9568c9d        8 days ago          179MB
+#default_sdk              latest              1067634b78dd        3 hours ago         361MB
 
 #run the docker image to verify it works locally
-#docker run -it disco_default:latest
+#docker run -it default_sdk:latest
 
 #Test the Python 
 #>>> import datetime
@@ -21,6 +20,6 @@ docker build -t disco_default_sdk -f default.dockerfile .
 #datetime.datetime(2020, 3, 19, 10, 58, 41, 657177)
 
 #push this to the repository (change this to your own path)
-#docker tag disco_default <docker_id>/disco_default:latest
-#docker push <docker_id>/disco_default:latest
+#docker tag default_sdk:latest <docker_id>/default_sdk:latest
+#docker push <docker_id>/default_sdk:latest
 
